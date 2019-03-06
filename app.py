5099169647b193d9b230ff3ff9f7fcc7874a8f72
jessefranklin <jessefrankling@gmail.com>
2019-03-06 17:23:07 -0500

    Added postgres and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -8,7 +10,7 @@ from resources.process import Process, ProcessList
 from resources.agent import Agent, AgentList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///client.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///client.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = ''
 api = Api(app)
